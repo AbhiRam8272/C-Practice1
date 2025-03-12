306c84b28a770353930f1728b3f448910d2f0387
AbhiRam8272 <144578772+AbhiRam8272@users.noreply.github.com>
2025-03-12 17:13:45 +0530

    Delete mine directory

--- mine/binary.h
+++
@@ -1,9 +0,0 @@
-void pb(unsigned int num) {
-    for (int i = 15; i >= 0; i--) {
-        printf("%d", (num >> i) & 1);
-        if (i % 4 == 0 && i != 0) {
-            printf(" "); // Add space for readability
-        }
-    }
-    printf("\n");
-} 
